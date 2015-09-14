7990372e4046aca53addb859de87b5b157bab59c
bhatiaPankaj <bhatia.pankaj92@gmail.com>
2015-09-14 23:27:54 +0530

    Cart+orders

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,49 +1,63 @@
-var blissArts=angular.module('blissArts', ['ngRoute','homePageController','productsController','aboutUsController','clientsController','testimonialsController']);
+var blissArts=angular.module('blissArts', ['ngRoute','customFilters','cart','homePageController','aboutUsController','clientsController','testimonialsController']);
+blissArts.constant("dataUrl", "http://localhost:2403/products");
+blissArts.constant("orderUrl", "http://localhost:2403/orders");
 blissArts.config(['$routeProvider',function($routeProvider){
 
 $routeProvider
 	.when('/',
 	{
-		controller:'homePageController',
-		templateUrl:'../views/homePage.html'
+		controller:'productController',
+		templateUrl:'../views/products.html'
 	}
 	)
-	.when('/products',
-	{
-		controller:'productsController',
-		templateUrl:'../views/products.html'
-	})
-	.when('/clients',
-	{
-		controller:'clientsController',
-		templateUrl:'../views/clients.html'
-	})
-	.when('/testimonials',
-	{
-		controller:'testimonialsController',
-		templateUrl:'../views/testimonials.html'
-	})
-	.when('/aboutUs',
-    {
-        controller: 'aboutUsController',
-        templateUrl: '../views/aboutUs.html'
-    }
-    )
-    .when('/contactUs',
-    {
-
-        templateUrl: '../views/contactUs.html'
-    }
-    )
-	.otherwise({redirectTo:'/'
+    .when('/checkout', {
+        templateUrl: '../views/checkoutSummary.html'
+    })
+    .when('/complete', {
+        templateUrl: "../views/thankYou.html"
+    })
+    .when('/placeOrder', {
+        templateUrl: "../views/placeOrder.html"
+    })
+    .otherwise({redirectTo:'/'
 	});
 	} ]);
 
 
+/*
 blissArts.controller('navController',['$scope', '$location', function ($scope, $location) {
         $scope.navClass = function (page) {
             var currentRoute = $location.path().substring(1) ;
             return page === currentRoute ? 'active' : '';
+        }}]);
+*/
+
+
+
+	blissArts.controller("productController", function ($scope,$http, dataUrl,orderUrl,$location,cart) {
+		$scope.data = {};
+		$http.get(dataUrl)
+			.success(function (data) {
+				$scope.data.products = data;
+			})
+			.error(function (error) {
+				$scope.data.error = error;
+			})
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete");
+                });
         }
 
-}]);
+	});
+
